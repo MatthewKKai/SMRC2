261692172348cb6fdeb196f04eefada8f5e79b25
Loafer <matthewzk@outlook.com>
2022-10-27 12:42:51 -0400

    update base function

--- model_utils/doc_level.py
+++ model_utils/doc_level.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
-from torch.nn import modules
+from torch.nn import Module
 
-class doc_level(modules.Module):
+class doc_level(Module):
+    def __init__(self, config):
+        super(doc_level)
+        self.config = config
+
+    def attention(self):
+        pass
---
+++ model_utils/entity_level.py
@@ -0,0 +1,13 @@
+from torch.nn import Module
+
+class entity_level(Module):
+    def __init__(self, config):
+        super(entity_level)
+        self.config = config
+
+    def entity_extraction(self):
+        pass
+
+    def graph_creation(self):
+        pass
+
