65dba7d098a038af9e50c4a55323387202946736
Loafer <matthewzk@outlook.com>
2022-11-14 13:24:49 -0500

    update configures

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import argparse
 
 
-
 def get_opt():
     parser = argparse.ArgumentParser()
 
     # add configs
     # ......
+
+    # paths
+    parser.add_argument()
+
+    # checkpoints
+    parser.add_argument()
 
     # text configs
     parser.add_argument()
