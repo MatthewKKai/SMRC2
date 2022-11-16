ae6daacdf1899b5d435e507f75d3225d53478fa0
Loafer <matthewzk@outlook.com>
2022-11-16 16:45:26 -0500

    update data preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,26 @@
 import pandas as pd
 import numpy as np
 import scispacy, spacy
+import pubmed_parser as pp
+
+def get_pmid():
+
+    return pmid
+
+def get_abs():
+
+    return abstract
+
+def get_intro():
+
+    return introduction
+
+def get_citanace():
+
+    return citance
+
+def get_citation():
+
+    return citation
+
+
