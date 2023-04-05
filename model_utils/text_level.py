01ebe09083136f48758152ecc58771ff6ea9b335
Loafer <matthewzk@outlook.com>
2023-04-05 13:58:43 -0400

    fix transformer block

--- model_utils/text_level.py
+++ model_utils/text_level.py
@@ -9,9 +9,10 @@ Config = config.get_opt()
 # convert doc into sentence-pairs
 class text_encoder(Module):
     def __init__(self, Config):
+        super(text_encoder, self).__init__()
         self.Config = Config
         self.tokenizer = BertTokenizer.from_pretrained(Config.version)
-        self.pretrain_model = BertModel.from_pretrained(Config.version) # add parameter later
+        self.pretrain_model = BertModel.from_pretrained(Config.version)
         self.transformer_layer = nn.TransformerEncoderLayer(d_model=Config.d_transformer, nhead=Config.nhead)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer=self.transformer_layer, num_layers=Config.num_layers)
         self.linear = nn.Linear(768, 512)
@@ -20,8 +21,8 @@ class text_encoder(Module):
         # get embedding
         inputs = self.tokenizer(text, max_length=512, truncation=True, padding=True, return_tensors='pt')
         initial_embedding = self.pretrain_model(**inputs)
-        initial_embedding_transit = self.transformer_encoder(initial_embedding['pooler_output'])
-        out = self.linear(initial_embedding_transit)
+        initial_embedding_transit = self.linear(initial_embedding['pooler_output'])
+        out = self.transformer_encoder(initial_embedding_transit)
 
         return out
 
