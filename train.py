4cdcc90ad8f60c2a594c6645f7234d51bc12f330
Loafer <matthewzk@outlook.com>
2023-04-05 13:58:00 -0400

    update training

--- train.py
+++ train.py
@@ -1,11 +1,28 @@
 from model_utils.text_level import text_encoder
 import config
 from torch import optim
+from torch import nn
+import torch
 from torch.utils.data import DataLoader
 from data_loader import create_dataset
 import os
 
 Config = config.get_opt()
+
+# label -> id
+labels2ids = {'cardiovascular diseases': 0,
+'chronic kidney disease': 1,
+'chronic respiratory diseases': 2,
+'ciabetes mellitus': 3,
+'cigestive diseases': 4,
+'hiv/aids': 5,
+'hepatitis a/b/c/e': 6,
+'mental disorders': 7,
+'musculoskeletal disorders': 8,
+'neoplasms (cancer)': 9,
+'neurological disorders': 10}
+
+label_onehot = nn.functional.one_hot(torch.tensor([0,1,2,3,4,5,6,7,8,9,10]), num_classes=11)
 
 def train():
     # params
@@ -13,7 +30,7 @@ def train():
         'batch_size': Config.batch_size,
         'shuffle': True
     }
-    epoch = 100
+    epoch = 10
 
     # prepare data
     dataset = create_dataset('./data.json')
@@ -22,22 +39,30 @@ def train():
     # load models
     textEncoder = text_encoder(Config)
 
+    # create optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(textEncoder.parameters(), lr=Config.learning_rate)
+
     # training
-    for abs, label in dataloader:
-        # print(abs)
-        text_embedding = textEncoder()
-        print(label)
-        print('------------')
+    for i in range(epoch):
+        total_loss = 0.0
+        print('----------Epoch {}---------'.format(i))
+        for abs, label in dataloader:
+            optimizer.zero_grad()
+            out = textEncoder(abs)
+            # print(out.shape)
+            label_id = torch.tensor([labels2ids[label[0]]])
+            # print(label_id)
+            loss = criterion(out, label_id)
+            loss.backward()
+            optimizer.step()
 
+            # print
+            total_loss += loss.item()
+            print('total_loss is: {}'.format(total_loss))
 
+    print("Finished")
 
-
-
-
-    optimizer = optim.AdamW()
-
-    # clear gradients
-    optimizer.zero_grad()
 
 if __name__=="__main__":
     train()
