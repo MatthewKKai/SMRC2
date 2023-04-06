b3f4cc0c387460f4a7dea5dcccfac4042df0ace1
Loafer <matthewzk@outlook.com>
2023-04-05 20:53:30 -0400

    update entity process

--- model_utils/entity_level.py
+++ model_utils/entity_level.py
@@ -4,15 +4,21 @@ import transformers
 from transformers import AutoModel, AutoTokenizer
 
 class entity_level(Module):
-    def __init__(self, config):
-        super(entity_level)
-        self.config = config
+    def __init__(self, Config):
+        super(entity_level, self).__init__()
+        self.config = Config
+        self.nlp = spacy.load(Config.ner_corpus)
 
-    def entity_extraction(self, doc):
-        return
 
-    def graph_creation(self):
+    def forward(self):
         pass
+
+    def entity_extraction(self, abs):
+        return self.nlp(abs).ents
+
+    def create_graph(self, abs):
+        entity_list = self.entity_extraction(abs)
+
 
     def masking(self):
         pass
