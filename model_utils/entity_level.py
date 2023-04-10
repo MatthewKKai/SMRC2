02adcca9ad623ff2e4ae151d14fe59f0b8b6684d
Loafer <matthewzk@outlook.com>
2023-04-09 22:07:57 -0400

    update entity level processor

--- model_utils/entity_level.py
+++ model_utils/entity_level.py
@@ -1,13 +1,15 @@
 from torch.nn import Module
 import scispacy, spacy
 import transformers
-from transformers import AutoModel, AutoTokenizer
+from transformers import BertTokenizer, BertModel
 
 class entity_level(Module):
     def __init__(self, Config):
         super(entity_level, self).__init__()
         self.config = Config
         self.nlp = spacy.load(Config.ner_corpus)
+        self.pretrained_model = BertModel.from_pretrained(Config.version)
+        self.tokenizer = BertTokenizer.from_pretrained(Config.version)
 
 
     def forward(self):
@@ -19,6 +21,3 @@ class entity_level(Module):
     def create_graph(self, abs):
         entity_list = self.entity_extraction(abs)
 
-
-    def masking(self):
-        pass
