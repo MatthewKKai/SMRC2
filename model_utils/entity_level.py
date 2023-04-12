1813f7396bdb7a194eb00446ee6a5d97011d24e8
Loafer <matthewzk@outlook.com>
2023-04-12 14:01:55 -0400

    update entity level processing

--- model_utils/entity_level.py
+++ model_utils/entity_level.py
@@ -1,7 +1,10 @@
 from torch.nn import Module
 import scispacy, spacy
 import transformers
+from model_utils.graph_utils import *
 from transformers import BertTokenizer, BertModel
+
+# entities_library_path = r'./entities2ids'
 
 class entity_level(Module):
     def __init__(self, Config):
@@ -10,14 +13,25 @@ class entity_level(Module):
         self.nlp = spacy.load(Config.ner_corpus)
         self.pretrained_model = BertModel.from_pretrained(Config.version)
         self.tokenizer = BertTokenizer.from_pretrained(Config.version)
+        self.gcn = nn.Sequential(
+            GCN(768, Config.gcn_dim),
+            GCN(Config.gcn_dim, 768)
+        )
+        self.fc = nn.Linear(768, 512)
 
 
-    def forward(self):
-        pass
+    def forward(self, abs):
+        entities = list(self.entity_extraction(abs))
+        entity_tokens = self.tokenizer(entities, max_length=100, truncation=True, padding=True, return_tensors='pt')
+        entity_initial_features = self.pretrained_model(**entity_tokens)
+        out, features = self.gcn(entity_initial_features['pooler_output'])
+
+        return self.fc(features)
+
 
     def entity_extraction(self, abs):
         return self.nlp(abs).ents
 
-    def create_graph(self, abs):
-        entity_list = self.entity_extraction(abs)
+    # def create_graph(self, abs):
+    #     entity_list = self.entity_extraction(abs)
 
