5b54ece141dd8c4651a14e85c7066a2cf7847cd7
Loafer <matthewzk@outlook.com>
2023-04-12 14:03:07 -0400

    update test_WD

--- test_WD.py
+++ test_WD.py
@@ -7,7 +7,6 @@ from transformers import BertModel, BertTokenizer
 from sklearn.decomposition import PCA
 import json
 
-# define PCA
 estimator = PCA(n_components=3)
 
 # abs1 = r'his review explores similarities between lymphocytes and cancer cells, and proposes a new model for the genesis of human cancer. we suggest that the development of cancer requires infection(s) during which antigenic determinants from pathogens mimicking self-antigens are co-presented to the immune system, leading to breaking t cell tolerance. some level of autoimmunity is normal and necessary for effective pathogen eradication. however, autoreactive t cells must be eliminated by apoptosis when the immune response is terminated. apoptosis can be deficient in the event of a weakened immune system, the causes of which are multifactorial. some autoreactive t cells suffer genomic damage in this process, but manage to survive. the resulting cancer stem cell still retains some functions of an inflammatory t cell, so it seeks out sites of inflammation inside the body. due to its defective constitutive production of inflammatory cytokines and other growth factors, a stroma is built at the site of inflammation similar to the temporary stroma built during wound healing. the cancer cells grow inside this stroma, forming a tumor that provides their vascular supply and protects them from cellular immune response.</p><p>as cancer stem cells have plasticity comparable to normal stem cells, interactions with surrounding normal tissues cause them to give rise to all the various types of cancers, resembling differentiated tissue types. metastases form at an advanced stage of the disease, with the proliferation of sites of inflammation inside the body following a similar mechanism. immunosuppressive cancer therapies inadvertently re-invigorate pathogenic microorganisms and parasitic infections common to cancer, leading to a vicious circle of infection, autoimmunity and malignancy that ultimately dooms cancer patients. based on this new understanding, we recommend a systemic approach to the development of cancer therapies that supports rather than antagonizes the immune system.'
@@ -17,7 +16,8 @@ estimator = PCA(n_components=3)
 # s1 = r"I love apple"
 # s2 = [abs1, abs2, abs3]
 
-# label -> color
+# label ->
+# define PCAcolor
 labels2color = {'cardiovascular diseases': 'red',
 'chronic kidney disease': 'purple',
 'chronic respiratory diseases': 'grey',
